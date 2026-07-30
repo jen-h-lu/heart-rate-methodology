586311121a199529d2112cdb2be244f2d1180d69
jennahlu <jenna.lu@ufl.edu>
2026-07-29 20:42:33 -0400

    Fix package loading dependencies

--- R/setup.R
+++ R/setup.R
@@ -1,30 +1,8 @@
-# Project setup ------------------------------------------------------------
+# Package dependencies
 
-required_packages <- c(
-  "tidyverse",
-  "here",
-  "renv",
-  "targets",
-  "tarchetypes",
-  "lme4",
-  "mgcv",
-  "fda",
-  "brms",
-  "future",
-  "furrr",
-  "patchwork",
-  "gt",
-  "devtools",
-  "usethis",
-  "testthat"
-)
-
-missing_packages <- required_packages[
-  !required_packages %in% installed.packages()[, "Package"]
-]
-
-if (length(missing_packages) > 0) {
-  install.packages(missing_packages)
-} else {
-  message("All required packages are already installed.")
-}
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(tibble)
+library(lme4)
+library(lmerTest)
